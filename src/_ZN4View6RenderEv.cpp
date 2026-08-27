df76d469e0812bfa85514d6be667b8b7126c0d8d
= <andrewboudreau@gmail.com>
2026-08-27 08:24:33 -0500

    Generate View methods from real C++

--- src/_ZN4View6RenderEv.cpp
+++ src/_ZN4View6RenderEv.cpp
@@ -1,21 +1,10 @@
-typedef int s32;
+//cpp
+#include "View.h"
 
-struct Matrix4x3 {
-    int data[12]; /* 0x30 bytes */
-};
+void CopyToViewMat(const Matrix4x3 *mat);
 
-struct dBase_c {
-    char pad[0x50];
-};
-
-struct View {
-    struct dBase_c base; /* 0x00 - 0x50 */
-    struct Matrix4x3 camMat;  /* 0x50 */
-};
-
-extern void _Z13CopyToViewMatPK9Matrix4x3(const struct Matrix4x3* mat);
-
-s32 _ZN4View6RenderEv(struct View* self) {
-    _Z13CopyToViewMatPK9Matrix4x3(&self->camMat);
+s32 View::Render()
+{
+    CopyToViewMat(&viewMat);
     return 1;
 }
