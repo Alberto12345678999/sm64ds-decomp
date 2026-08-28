280ba2e961b3583dd6ab52a6d3730c9580304775
= <andrewboudreau@gmail.com>
2026-08-28 09:24:27 -0500

    Use compiler-owned dActor list-node lifetime

--- src/_ZN9fLiNdBa_cD1Ev.cpp
+++ src/_ZN9fLiNdBa_cD1Ev.cpp
@@ -1,8 +1,11 @@
-extern int data_020a4b68;
-unsigned int func_02044104(int v)
+//cpp
+// @symbol _ZN9fLiNdBa_cD1Ev
+#include "fListNode.h"
+
+extern "C" fLiNdBa_c *data_020a4b68;
+
+fLiNdBa_c::~fLiNdBa_c()
 {
-  if (data_020a4b68 == v)
-  {
-    data_020a4b68 = (data_020a4b68 == v) * 0;
-  }
+    if (data_020a4b68 == this)
+        data_020a4b68 = 0;
 }
